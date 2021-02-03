64e8103f7f5d940befec52caf1326c69047785c8
Denis <denislee98@gmail.com>
2021-02-03 23:24:11 +0300

    'login'

--- vue/src/views/login/Login.vue
+++ vue/src/views/login/Login.vue
@@ -1,21 +1,37 @@
 <template>
-	<div>
-		<div class="modal-form" v-if="visLogin">
-			<input type="text" v-model="user.username" placeholder="Почта">
-			<input type="text" v-model="user.password" placeholder="Пароль">
-			<div class="modal-form-btns">
-				<button class="btn-submit" style="margin-left: 10px;" v-on:click="login()">Войти</button>
-				<button class="btn-submit" style="margin-left: 10px;" v-on:click="visLogin=!visLogin">Регистрация</button>
+	<div class="login-align">
+		<div class="login-center">
+			<div class="modal-form" v-if="visLogin">
+				<div class="login-logo">
+					<span class="logo-1">Lingua</span><span class="logo-2">Trash</span>
+				</div>
+				<div class="login-text-css">Комфортное обучение</div>
+				<input type="text" v-model="user.username" placeholder="Почта">
+				<input type="text" v-model="user.password" placeholder="Пароль">
+				<div>
+				</div>
+				<div class="modal-form-btns" style="margin-top: 20px;">
+					<button class="button-add" v-on:click="login()">Войти</button>
+					<button class="button-add" v-on:click="visLogin=!visLogin">Создать аккаунт</button>
+				</div>
+			</div>
+
+			<form @submit.prevent="signup" class="modal-form" v-if="!visLogin">
+				<input type="text" v-model="user.username" placeholder="Почта">
+				<input type="text" v-model="user.password" placeholder="Пароль">
+				<div class="modal-form-btns">
+					<button class="btn-submit" style="margin-left: 10px;" type="submit">Регистрация</button>
+				</div>
+			</form>
+
+			<div class="login-text">
+				<div class="login-text-style">
+					<span>Добрый вечер!</span> <br>
+					Вы случайно наткнулись на платформу для запоминания иностранных слов. <br><br>
+					Для доступа к платформе нужно всего лишь "Войти"
+				</div>
 			</div>
 		</div>
-
-		<form @submit.prevent="signup" class="modal-form" v-if="!visLogin">
-			<input type="text" v-model="user.username" placeholder="Почта">
-			<input type="text" v-model="user.password" placeholder="Пароль">
-			<div class="modal-form-btns">
-				<button class="btn-submit" style="margin-left: 10px;" type="submit">Регистрация</button>
-			</div>
-		</form>
 	</div>
 </template>
 
