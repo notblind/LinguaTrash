ae08afc98e71ec738f9473aee822db4cdf386cc0
Denis Li <denislee98@gmail.com>
2023-11-29 01:01:20 +0100

    added first test

--- vocabulary/tests.py
+++ vocabulary/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from rest_framework.status import HTTP_200_OK
 
-# Create your tests here.
+from vocabulary.models import Vocabulary
+
+
+class ApiTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_get_vocabulary(self):
+        Vocabulary.objects.create(name="Vocabulary #1")
+        Vocabulary.objects.create(name="Vocabulary #2")
+
+        response = self.client.get("/vocabulary/api/v1/vocabulary")
+        self.assertEqual(response.status_code, HTTP_200_OK)
+
+        self.assertContains(response, "Vocabulary #1")
+        self.assertContains(response, "Vocabulary #2")
